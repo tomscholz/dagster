8d6aff9e00adcf02d3354560bfa7e9eba001ac79
benpankow <ben@elementl.com>
2023-05-24 17:25:14 +0400

    fix tests

--- python_modules/dagster/dagster_tests/conftest.py
+++ python_modules/dagster/dagster_tests/conftest.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 
 import dagster._seven as seven
+import pytest
+from dagster._core.definitions.decorators.op_decorator import CODE_ORIGIN_ENABLED
 
 IS_BUILDKITE = os.getenv("BUILDKITE") is not None
 
@@ -11,3 +13,12 @@ IS_BUILDKITE = os.getenv("BUILDKITE") is not None
 # Fixed in later versions of Python but never back-ported, see the bug for details.
 if seven.IS_WINDOWS and sys.version_info[0] == 3 and sys.version_info[1] == 6:
     subprocess._cleanup = lambda: None  # type: ignore # noqa: SLF001
+
+
+@pytest.fixture
+def ignore_code_origin():
+    CODE_ORIGIN_ENABLED[0] = False
+
+    yield
+
+    CODE_ORIGIN_ENABLED[0] = True
--- python_modules/dagster/dagster_tests/core_tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from dagster._core.definitions.decorators.op_decorator import CODE_ORIGIN_ENABLED
-
-
-@pytest.fixture
-def ignore_code_origin():
-    CODE_ORIGIN_ENABLED[0] = False
